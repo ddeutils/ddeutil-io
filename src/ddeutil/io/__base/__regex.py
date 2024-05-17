6e978655350e7f38118f8d79971a13bf40e0ffdb
Korawich Anuttra <korawich.anu@gmail.com>
2024-05-17 22:54:57 +0700

    :construction: refactored: change constructure of regex conf class.

--- src/ddeutil/io/__base/__regex.py
+++ src/ddeutil/io/__base/__regex.py
@@ -16,7 +16,7 @@ from re import (
 class RegexConf:
     """Core Configuration Model"""
 
-    # Normal regular expression for the secret value.
+    # NOTE: Normal regular expression for the secret value.
     # ---
     # [\"\']?                             # single or double quoted value
     # (?P<search>@secrets{                # search string for replacement
@@ -25,7 +25,7 @@ class RegexConf:
     # })                                  # end with }
     # [\"\']?                             # single or double-quoted value
     #
-    # Note: For secrets grouping level.
+    # NOTE: For secrets grouping level.
     # ---
     # [\"\']?                             # single or double quoted value
     # (?P<search>@secrets                 # search string for replacement
@@ -34,19 +34,19 @@ class RegexConf:
     #     (:(?P<braced_default>.*?))?     # value default with sep :
     # })                                  # end with }
     # [\"\']?                             # single or double-quoted value
-    RE_SECRETS: Pattern = re.compile(
-        r"""
+    __re_secrets: str = r"""
         [\"\']?                             # single or double quoted value
         (?P<search>@secrets{                # search string for replacement
             (?P<braced>.*?)                 # value if use braced {}
             (?::(?P<braced_default>.*?))?   # value default with sep :
         })                                  # end with }
         [\"\']?                             # single or double-quoted value
-    """,
-        MULTILINE | UNICODE | IGNORECASE | VERBOSE,
+    """
+    RE_SECRETS: Pattern = re.compile(
+        __re_secrets, MULTILINE | UNICODE | IGNORECASE | VERBOSE
     )
 
-    # Normal regular expression for the function value.
+    # NOTE: Normal regular expression for the function value.
     # ---
     # [\"\']?                             # single or double quoted value
     # (?P<search>@function{               # search string for replacement
@@ -54,23 +54,22 @@ class RegexConf:
     #     (?::(?P<arguments>.*?))?        # arguments for calling function
     # })                                  # end with }
     # [\"\']?                             # single or double-quoted value
-    RE_FUNCTION: Pattern = re.compile(
-        r"""
+    __re_function: str = r"""
         [\"\']?                             # single or double quoted value
         (?P<search>@function{               # search string for replacement
             (?P<function>[\w.].*?)          # called function
             (?::(?P<arguments>.*?))?        # arguments for calling function
         })                                  # end with }
         [\"\']?                             # single or double-quoted value
-    """,
-        MULTILINE | UNICODE | IGNORECASE | VERBOSE,
+    """
+    RE_FUNCTION: Pattern = re.compile(
+        __re_function, MULTILINE | UNICODE | IGNORECASE | VERBOSE
     )
 
-    # Normal regular expression for dotenv variable
+    # NOTE: Normal regular expression for dotenv variable
     # ---
     # (\\)?(\$)({?([A-Z0-9_]+)}?)
-    RE_DOTENV_VAR: Pattern = re.compile(
-        r"""
+    __re_dotenv_var: str = r"""
         (\\)?               # is it escaped with a backslash?
         (\$)                # literal $
         (                   # collect braces with var for sub
@@ -78,19 +77,17 @@ class RegexConf:
             ([A-Z0-9_]+)    # match the variable
             }?              # closing brace
         )                   # braces end
-    """,
-        IGNORECASE | VERBOSE,
-    )
+    """
+    RE_DOTENV_VAR: Pattern = re.compile(__re_dotenv_var, IGNORECASE | VERBOSE)
 
-    # Normal regular expression for dotenv
+    # NOTE: Normal regular expression for dotenv
     # ---
     # ^\s*(?:export\s+)?(?P<name>[\w.-]+)(?:\s*=\s*?|:\s+?)(?P<value>\s*\'(?:\\'|[^'])*\'|\s*\"(?:\\"|[^"])*\"
     # |\s*`(?:\\`|[^`])*`|[^#\r\n]+)?\s*$
-    RE_DOTENV: Pattern = re.compile(
-        r"""
-        ^\s*(?:export\s+)?      # optional export
-        (?P<name>[\w.-]+)       # name of key
-        (?:\s*=\s*?|:\s+?)      # separator `=` or `:`
+    __re_dotenv: str = r"""
+        ^\s*(?:export\s+)?          # optional export
+        (?P<name>[\w.-]+)           # name of key
+        (?:\s*=\s*?|:\s+?)          # separator `=` or `:`
         (?P<value>
             \s*\'(?:\\'|[^'])*\'    # single quoted value
             |
@@ -98,27 +95,25 @@ class RegexConf:
             |
             \s*`(?:\\`|[^`])*`      # backticks value
             |
-            [^#\r\n]+           # unquoted value
-        )?\s*                   # optional space
+            [^#\r\n]+               # unquoted value
+        )?\s*                       # optional space
         (?:[^\S\r\n]*\#[^\r\n]*)?
         $
-    """,
-        MULTILINE | VERBOSE,
-    )
+    """
+    RE_DOTENV: Pattern = re.compile(__re_dotenv, MULTILINE | VERBOSE)
 
-    # Note
+    # NOTE:
     # ---
     # (\s|^)#.*
+    __re_yaml_comment: str = r"(\s|^)#.*"
     RE_YAML_COMMENT: Pattern = re.compile(
-        r"(\s|^)#.*",
-        MULTILINE | UNICODE | IGNORECASE,
+        __re_yaml_comment, MULTILINE | UNICODE | IGNORECASE
     )
 
-    # Note
+    # NOTE:
     # ---
     # [\"\']?(\$(?:(?P<escaped>\$|\d+)|({(?P<braced>.*?)(:(?P<braced_default>.*?))?})))[\"\']?
-    RE_ENV_SEARCH: Pattern = re.compile(
-        r"""
+    __re_env_search: str = r"""
         [\"\']?                             # single or double quoted value
         (\$(?:                              # start with non-capturing group
             (?P<escaped>\$|\d+)             # escape $ or number like $1
@@ -129,18 +124,22 @@ class RegexConf:
             })
         ))
         [\"\']?                             # single or double quoted value
-    """,
-        MULTILINE | UNICODE | IGNORECASE | VERBOSE,
+    """
+    RE_ENV_SEARCH: Pattern = re.compile(
+        __re_env_search, MULTILINE | UNICODE | IGNORECASE | VERBOSE
     )
 
-    RE_ENV_VALUE_QUOTED: Pattern = re.compile(
-        r"""
+    __re_env_value_quoted: str = r"""
         ^
-            (?P<quoted>[\'\"`])
+            (?P<quoted>[\'\"`])     # single or double quoted value
             (?P<value>.*)\1
         $
-    """,
-        MULTILINE | VERBOSE,
+    """
+    RE_ENV_VALUE_QUOTED: Pattern = re.compile(
+        __re_env_value_quoted,
+        MULTILINE | UNICODE | VERBOSE,
     )
 
-    RE_ENV_ESCAPE: Pattern = re.compile(r"\\([^$])")
+    # NOTE:
+    __re_env_escape: str = r"\\([^$])"
+    RE_ENV_ESCAPE: Pattern = re.compile(__re_env_escape, MULTILINE | UNICODE)
